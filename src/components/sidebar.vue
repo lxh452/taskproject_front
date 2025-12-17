a5acf5c9439183a67c80970eeb068bf255ff3371
xhliu <xhliu@gizwits.com>
2025-12-17 14:15:40 +0800

    更改

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -4,11 +4,7 @@
         <div class="sidebar-logo">
             <div class="logo-wrapper">
                 <div class="logo-icon">
-                    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M12 2L2 7L12 12L22 7L12 2Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M2 17L12 22L22 17" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        <path d="M2 12L12 17L22 12" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                    </svg>
+                    <img src="@/assets/img/logo.jpg" alt="Logo" class="logo-img" />
                 </div>
                 <transition name="fade">
                     <span class="logo-text" v-if="!sidebar.collapse">Task Pro</span>
